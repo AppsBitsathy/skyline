d4a48d57afc1a2cfd92f1a4d9a270dcd01b199c6
Tharun-GS <tharunbca36@gmail.com>
2022-05-12 15:57:35 +0530

    env changes

--- resources/views/includes/top.blade.php
+++ resources/views/includes/top.blade.php
@@ -1,5 +1,5 @@
 <meta name="csrf-token" content="{{ csrf_token() }}" />
-<link href="url{{ asset('assets/fonts/material-icons.css') }}" rel="stylesheet">
-<link href="url{{ asset('assets/css/materialize.min.css') }}" type="text/css" rel="stylesheet"
+<link href="{{ asset('assets/fonts/material-icons.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/materialize.min.css') }}" type="text/css" rel="stylesheet"
     media="screen,projection" />
-<link href="url{{ asset('assets/css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection" />
+<link href="{{ asset('assets/css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection" />
